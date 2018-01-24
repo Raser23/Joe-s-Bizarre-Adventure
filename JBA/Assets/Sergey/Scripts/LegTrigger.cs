d015dddd0ba49f95b6b601c89b90326d57390209
Raser23 <ser.neshadim@yandex.ru>
2018-01-24 21:14:21 +0700

    HUI

--- JBA/Assets/Sergey/Scripts/LegTrigger.cs
+++ JBA/Assets/Sergey/Scripts/LegTrigger.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class LegTrigger : MonoBehaviour {
 
-    public bool triggered
-    {
-        get { return ins.Count > 0; }
+    public bool triggered { get
+        {
+            return ins.Count > 0;
+        } 
     }
 
+
     public List<Collider> ins;
+
+    /*private void FixedUpdate()
+    {
+        Collider[] cols =Physics.OverlapSphere(transform.position,gameObject.GetComponent<SphereCollider>().radius);
+
+        foreach(Collider col in cols){
+            if(col.transform.tag != "Player"){
+                triggered = true;
+                return;
+            }
+        }
+        triggered = false
+
+    }*/
 
     void Start(){
         ins = new List<Collider>();
