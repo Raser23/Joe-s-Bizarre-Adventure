514870a53094a5d3fb68150f5364003a2d1df088
Andronchanskiy <31404779+Andronchanskiy@users.noreply.github.com>
2018-01-25 12:07:16 +0700

    Генерация дверей между комнатами

--- JBA/Assets/Andrey/Scripts/Generation.cs
+++ JBA/Assets/Andrey/Scripts/Generation.cs
@@ -5,10 +5,16 @@ using System;
 
 public class Generation : MonoBehaviour
 {
+    public struct Room
+    {
+        public Vector2 position;
+        public GameObject obj;
+    }
     public GameObject example;
     public int number_of_rooms = 100;
     public List<Vector2> possible_points = new List<Vector2>();
     public List<Vector2> points = new List<Vector2>();
+    public List<Room> rooms = new List<Room>();
     public Vector2 trash;
     // Use this for initialization
     void Start()
@@ -37,36 +43,45 @@ public class Generation : MonoBehaviour
                 a -= Weight(possible_points[counter]);
             }
             points.Add(possible_points[counter]);
-            GameObject instance = Instantiate(Resources.Load("Room_template", typeof(GameObject))) as GameObject;
             trash = possible_points[counter];
+            GameObject instance = Instantiate(Resources.Load("Room_template", typeof(GameObject))) as GameObject;
             instance.transform.position = new Vector3(trash.x * 20, 100, trash.y * 20);
-            foreach (Transform child in instance.transform)
-            {
-                if (child.tag == "Marker")
-                {
-                    switch (child.name)
-                    {
-                        case "Wall_Position_Z+":
-                        
-                            break;
-                        case "Wall_Position_Z-":
-                             
-                            break;
-                        case "Wall_Position_Z-":
+            Room trash_room;
+            trash_room.position = trash;
+            trash_room.obj = instance;
+            rooms.Add(trash_room);
+            /*         GameObject instance = Instantiate(Resources.Load("Room_template", typeof(GameObject))) as GameObject;
+                     instance.transform.position = new Vector3(trash.x * 20, 100, trash.y * 20);
+                     foreach (Transform child in instance.transform)
+                     {
+                         if (child.tag == "Marker")
+                         {
+                             Vector2 vec;
+                             switch (child.name)
+                             {
+                                 case "Wall_Position_Z+":
+                                     vec.x = 0;
+                                     vec.z = 1;
+                                 if (points.IndexOf(bt) == -1)
+                                     break;
+                                 case "Wall_Position_Z-":
 
-                            break;
-                        case "Wall_Position_Z-":
+                                     break;
+                                 case "Wall_Position_Z-":
 
-                            break;
-                        default:
-                            break;
-                    }
-                    GameObject instance_wall = Instantiate(Resources.Load("Wall_template", typeof(GameObject))) as GameObject;
-                    instance_wall.transform.position = child.transform.position;
-                    instance_wall.transform.rotation = child.transform.rotation;
-                    instance_wall.transform.parent = child;
-                }
-            }
+                                     break;
+                                 case "Wall_Position_Z-":
+
+                                     break;
+                                 default:
+                                     break;
+                             }
+                             GameObject instance_wall = Instantiate(Resources.Load("Wall_template", typeof(GameObject))) as GameObject;
+                             instance_wall.transform.position = child.transform.position;
+                             instance_wall.transform.rotation = child.transform.rotation;
+                             instance_wall.transform.parent = child;
+                         }
+                     }*/
 
             List<Vector2> n = new List<Vector2>() { new Vector2(1, 0), new Vector2(0, 1), new Vector2(-1, 0), new Vector2(0, -1) };
 
@@ -99,6 +114,7 @@ public class Generation : MonoBehaviour
             possible_points.Remove(trash);
             number_of_rooms--;
         }
+        Wall_generation();
     }
 
     // Update is called once per frame
@@ -107,6 +123,91 @@ public class Generation : MonoBehaviour
 
     }
 
+ /*   void Room_creation(Vector2 trash)
+    {
+        GameObject instance = Instantiate(Resources.Load("Room_template", typeof(GameObject))) as GameObject;
+        instance.transform.position = new Vector3(trash.x * 20, 100, trash.y * 20);
+    }
+*/
+    void Wall_generation()
+    {
+        foreach (Room room in rooms)
+        {
+            foreach (Transform child in room.obj.transform)
+            if (child.tag == "Marker")
+            {
+                Vector2 vec;
+                GameObject instance_wall;
+                switch (child.name)
+                {
+                    case "Wall_Position_Z+":
+                        vec.x = 0;
+                        vec.y = 1;
+                        if (points.IndexOf(room.position + vec) == -1)
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_template", typeof(GameObject))) as GameObject;
+                        }
+                        else
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_Door_template", typeof(GameObject))) as GameObject;
+                        }
+                        instance_wall.transform.position = child.transform.position;
+                        instance_wall.transform.rotation = child.transform.rotation;
+                        instance_wall.transform.SetParent(child.transform);
+                        break;
+                    case "Wall_Position_Z-":
+                        vec.x = 0;
+                        vec.y = -1;
+                        if (points.IndexOf(room.position + vec) == -1)
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_template", typeof(GameObject))) as GameObject;
+                        }
+                        else
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_Door_template", typeof(GameObject))) as GameObject;
+                        }
+                        instance_wall.transform.position = child.transform.position;
+                        instance_wall.transform.rotation = child.transform.rotation;
+                        instance_wall.transform.SetParent(child.transform);
+                        break;
+                    case "Wall_Position_X+":
+                        vec.x = 1;
+                        vec.y = 0;
+                        if (points.IndexOf(room.position + vec) == -1)
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_template", typeof(GameObject))) as GameObject;
+                        }
+                        else
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_Door_template", typeof(GameObject))) as GameObject;
+                        }
+                        instance_wall.transform.position = child.transform.position;
+                        instance_wall.transform.rotation = child.transform.rotation;
+                        instance_wall.transform.SetParent(child.transform);
+                        break;
+                    case "Wall_Position_X-":
+                        vec.x = -1;
+                        vec.y = 0;
+                        if (points.IndexOf(room.position + vec) == -1)
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_template", typeof(GameObject))) as GameObject;
+                        }
+                        else
+                        {
+                            instance_wall = Instantiate(Resources.Load("Wall_Door_template", typeof(GameObject))) as GameObject;
+                        }
+                        instance_wall.transform.position = child.transform.position;
+                        instance_wall.transform.rotation = child.transform.rotation;
+                        instance_wall.transform.SetParent(child.transform);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    
     int Weight(Vector2 a)
     {
         return (int)(1000 * number_of_rooms / (Mathf.Sqrt(a.x * a.x + a.y * a.y) + 1));
